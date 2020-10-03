2101f0a25646a2e7df57eca8218a1298294edc08
abdallah afar <abdallahzuber@outlook.com>
2020-10-03 16:31:38 +0300

    Add project files.

---
+++ Project calculator program/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Project_calculator_program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ed_n1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ed_n2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_reslut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ed_n1
+            // 
+            this.ed_n1.Location = new System.Drawing.Point(26, 118);
+            this.ed_n1.Margin = new System.Windows.Forms.Padding(6);
+            this.ed_n1.Name = "ed_n1";
+            this.ed_n1.Size = new System.Drawing.Size(135, 33);
+            this.ed_n1.TabIndex = 0;
+            this.ed_n1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number 1";
+            // 
+            // ed_n2
+            // 
+            this.ed_n2.Location = new System.Drawing.Point(197, 118);
+            this.ed_n2.Margin = new System.Windows.Forms.Padding(6);
+            this.ed_n2.Name = "ed_n2";
+            this.ed_n2.Size = new System.Drawing.Size(135, 33);
+            this.ed_n2.TabIndex = 0;
+            this.ed_n2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(213, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number 2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 244);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(137, 244);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(249, 244);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 34);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "/";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(193, 244);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 34);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "*";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Result";
+            // 
+            // lb_reslut
+            // 
+            this.lb_reslut.Location = new System.Drawing.Point(138, 369);
+            this.lb_reslut.Name = "lb_reslut";
+            this.lb_reslut.Size = new System.Drawing.Size(105, 25);
+            this.lb_reslut.TabIndex = 1;
+            this.lb_reslut.Text = "00";
+            this.lb_reslut.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 516);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lb_reslut);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ed_n2);
+            this.Controls.Add(this.ed_n1);
+            this.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ed_n1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ed_n2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_reslut;
+    }
+}
+
---
+++ Project calculator program/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_calculator_program
+{
+    public partial class Form1 : Form
+    {
+        double n1;
+        double n2;
+        double reslut;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDouble(ed_n1.Text);
+            n2 = Convert.ToDouble(ed_n2.Text);
+            reslut = n1 + n2;
+            lb_reslut.Text = Convert.ToString(reslut);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDouble(ed_n1.Text);
+            n2 = Convert.ToDouble(ed_n2.Text);
+            reslut = n1 - n2;
+            lb_reslut.Text = Convert.ToString(reslut);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDouble(ed_n1.Text);
+            n2 = Convert.ToDouble(ed_n2.Text);
+            reslut = n1 * n2;
+            lb_reslut.Text = Convert.ToString(reslut);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDouble(ed_n1.Text);
+            n2 = Convert.ToDouble(ed_n2.Text);
+            reslut = n1 / n2;
+            lb_reslut.Text = Convert.ToString(reslut);
+        }
+    }
+}
